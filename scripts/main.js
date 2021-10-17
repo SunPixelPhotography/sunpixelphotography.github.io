05c37985e9dd8ff10787f76b507a1ba43a093230
iam369 <ancodia.net@gmail.com>
2021-10-17 13:18:46 +0200

    Fixes
    - dropdown css
    - google maps

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,32 @@
-{% include 'LFo/SiteFeatureLocalStorage' %}
-{% include 'LFo/SiteFeatureWishlist' %}
-{% include 'LFo/SiteFeatureLastViewedItems' %}
+
+	
+	
+function LFoStorageData(key, dflt_val) {
+	var items = dflt_val;
+	var data = window.localStorage.getItem('photography.sunpixel.' + key);
+	if (data) {
+		try {
+			items = JSON.parse(data);
+		}
+		catch(err) {
+			items = dflt_val;
+		}
+	} else {
+		items = dflt_val;
+	}
+	return items;
+}
+function LFoLocalStorageSave(key, val) {
+	window.localStorage.setItem('photography.sunpixel.' + key, JSON.stringify(val));
+}
+	
+
+
+
+	
+
+
+
+	
+
+
